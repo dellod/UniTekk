38035f2997aaec6c7908f62084332c26e195491c
Daryl Dang <43867408+dellod@users.noreply.github.com>
2020-04-08 15:44:56 -0600

    Added SQL change product details.

---
+++ UniTekk/SQLQueries/changeProductDetails.sql
@@ -0,0 +1,32 @@
+﻿create procedure changeProductDetails(
+@returnValue as int OUTPUT,
+@type as varchar(50),
+@username as varchar(50),
+@productId as int,
+@productName as varchar(50),
+@price as int,
+@availability as int
+)
+AS
+BEGIN
+	IF @type = 'name'
+	BEGIN
+		UPDATE Product
+		SET name = @productName
+		WHERE @productId = Product.productID
+	END
+	ELSE IF @type = 'price'
+	BEGIN
+		UPDATE Sells
+		SET Sells.price = @price
+		WHERE @productId = Sells.productID
+	END
+	ELSE IF @type = 'availability'
+	BEGIN
+		UPDATE Sells
+		SET Sells.availability = @availability
+		WHERE @productId = Sells.productID
+	END
+	SET @returnValue = 1;
+	RETURN @returnValue;
+END;
